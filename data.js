301cba8beaff24a739a2773e047a5442eebee95f
Maha <imahaalnefaie@gmail.com>
2026-06-23 06:08:39 +0300

    Modify book data and add new Arabic titles
    Updated book entries and added new titles in Arabic.

--- data.js
+++ data.js
@@ -13,63 +13,92 @@ const books = [
         title: "The Dremer",
         price: 50,
         image: "./img/The Dremer.jpeg",
-        author: "Jane Smith",
-        bestSeller: false,
-        newArrival: true
-    },
-    {
-        id: 3,
-        title: "The Adventures of Sherlock Holmes",
-        price: 50,
-        image: "https://covers.openlibrary.org/b/id/8226191-L.jpg",
-        author: "Arthur Conan Doyle",
+        author: "John Doe",
         bestSeller: true,
         newArrival: false
+
     },
+      {
+
+    id: 3,
+    title: "رحلات جلفر",
+    price: 45,
+    image: "https://covers.openlibrary.org/b/id/6979861-L.jpg",
+    author: "جوناثان سويفت",
+    bestSeller: false,
+    newArrival: true
+
+  },
+
+  {
+
+    id: 4,
+    title: "روبنسون كروزو",
+    price: 50,
+    image: "https://covers.openlibrary.org/b/id/7222246-L.jpg",
+    author: "دانيال ديفو",
+    bestSeller: true,
+    newArrival: false
+
+  },
+
+  {
+
+    id: 5,
+    title: "حول العالم في 80 يومًا",
+    price: 65,
+    image: "https://covers.openlibrary.org/b/id/8231856-L.jpg",
+    author: "جول فيرن",
+    bestSeller: false,
+    newArrival: true
+
+  },
     {
-        id: 4,
-        title: "Dracula",
-        price: 60,
-        image: "https://covers.openlibrary.org/b/id/8231990-L.jpg",
-        author: "Bram Stoker",
-        bestSeller: true,
-        newArrival: false
-    },
-    {
-        id: 5,
-        title: "Frankenstein",
-        price: 98,
-        image: "https://covers.openlibrary.org/b/id/8232171-L.jpg",
-        author: "Mary Shelley",
-        bestSeller: false,
-        newArrival: false
-    },
-    {
-        id: 6,
-        title: "The Picture of Dorian Gray",
-        price: 55,
-        image: "https://covers.openlibrary.org/b/id/8235116-L.jpg",
-        author: "Oscar Wilde",
-        bestSeller: false,
-        newArrival: false
-    },
-    {
-        id: 7,
-        title: "The Time Machine",
-        price: 70,
-        image: "https://covers.openlibrary.org/b/id/8234145-L.jpg",
-        author: "H. G. Wells",
-        bestSeller: false,
-        newArrival: false
-    },
-    {
-        id: 8,
-        title: "Treasure Island",
-        price: 30,
-        image: "https://covers.openlibrary.org/b/id/8235084-L.jpg",
-        author: "Robert Louis Stevenson",
-        bestSeller: false,
-        newArrival: false
-    }
+
+    id: 5,
+    title: "فرانكنشتاين",
+    price: 98,
+    image: "https://covers.openlibrary.org/b/id/8232171-L.jpg",
+    author: "ماري شيلي",
+    bestSeller: false,
+    newArrival: false
+
+},
+
+{
+
+    id: 6,
+    title: "صورة دوريان غراي",
+    price: 55,
+    image: "https://covers.openlibrary.org/b/id/8235116-L.jpg",
+    author: "أوسكار وايلد",
+    bestSeller: false,
+    newArrival: false
+
+},
+
+{
+
+    id: 7,
+    title: "آلة الزمن",
+    price: 70,
+    image: "https://covers.openlibrary.org/b/id/8234145-L.jpg",
+    author: "هـ. ج. ويلز",
+    bestSeller: false,
+    newArrival: false
+
+},
+
+{
+
+    id: 8,
+    title: "جزيرة الكنز",
+    price: 30,
+    image: "https://covers.openlibrary.org/b/id/8235084-L.jpg",
+    author: "روبرت لويس ستيفنسون",
+    bestSeller: false,
+    newArrival: false
+
+}
 
 ]; 
